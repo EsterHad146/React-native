4de87a7575facf0f2dc50bb1036a2e7ac446b7ae
EsterHad146 <hadassaester146@gmail.com>
2025-01-28 16:23:58 -0300

    Update App2.tsx

--- App2.tsx
+++ App2.tsx
@@ -12,7 +12,7 @@ class App extends Component {
         <Text style={{ color: "#FF0000" , fontSize: 25, margin: 15}}>TII 4</Text>
         <Text style={{ fontSize:30 }}> {nome} </Text>
 
-        <IA largura={100} altura={200} fulano="Jarvis" />
+        <IA largura={300} altura={200} fulano="Jarvis" />
 
       </View>
     );
