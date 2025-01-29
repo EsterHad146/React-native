380920c988ee9611e01473d1b1f49a071076ac1f
EsterHad146 <hadassaester146@gmail.com>
2025-01-29 16:28:54 -0300

    Update Contador.jsx

--- Contador.jsx
+++ Contador.jsx
@@ -1,34 +1,49 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-class App extends Component{
-    constructor(props){
+class App extends Component {
+    constructor(props) {
         super(props); //serve como parametro para a class Component
         this.state = {
             contador: 0,
         };
     }
 
-    aumentarClique = () =>{
-        this.setState({ contador: this.state.contador + 1});
-    };
-    diminuirClique = () =>{
-        this.setState({ contador: this.state.contador - 1});
+    // aumentarClique = () =>{
+    //     this.setState({ contador: this.state.contador + 1});
+    // };
+    // diminuirClique = () =>{
+    //         this.setState({ contador: this.state.contador - 1});
+    // };
+
+   //***MODO SIMPLIFICADO*** 
+    contarClique = (n) => {
+        this.setState({ contador: this.state.contador + n });
     };
 
-    render(){
-        return(
-            <View style ={{padding: 50}}>
+
+
+    render() {
+        return (
+            <View style={{ padding: 50 }}>
                 <View> {/*  style={styles.container} */}
                     <Text>Você clicou {this.state.contador} vezes</Text>
                 </View>
-                <TouchableOpacity onPress={this.aumentarClique}>
+
+                {/* <TouchableOpacity onPress={this.aumentarClique}>
                     <Text>Somar (+)</Text>
                 </TouchableOpacity>
                 <TouchableOpacity onPress={this.diminuirClique}>
                     <Text>Subtrair (-)</Text>
-                </TouchableOpacity>
+                </TouchableOpacity> */}
 
+                {/* MODO SIMPLIFICADO */}
+                <TouchableOpacity onPress={()=>this.contarClique (1)}>
+                    <Text>Somar (+)</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={()=>this.contarClique (-1)}>
+                    <Text>Subtrair (-)</Text>
+                </TouchableOpacity>
             </View>
         )
     }
