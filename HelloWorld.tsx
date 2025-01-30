496f2a6ce9043bc18d549cae9792dc72e3ab41e9
EsterHad146 <hadassaester146@gmail.com>
2025-01-30 14:23:21 -0300

    Create HelloWorld.tsx

---
+++ HelloWorld.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+function App(){
+  return (
+    <View>
+  <Text style={{fontSize : 50, color:"pink", margin : 50 }}>Olá Mundo!</Text>
+    </View>
+  )
+}
+export default App
