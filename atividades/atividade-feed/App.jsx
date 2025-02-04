2e879af633aefb6d22769820104540a472428dc4
EsterHad146 <hadassaester146@gmail.com>
2025-02-04 17:26:42 -0300

    Add files via upload

--- atividades/atividade-feed/App.jsx
+++ atividades/atividade-feed/App.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import { View, StyleSheet, FlatList } from "react-native";
+import { View, StyleSheet, FlatList, Image, TextButton,Text } from "react-native";
 import Pessoa from "./src/Pessoas";
 
 class App extends Component{
@@ -24,11 +24,14 @@ class App extends Component{
   render(){
     return(
       <View style={styles.container}>
+        <Img />
+        <Text>Confira as informações sobre os personagens!</Text>
+        <TextButton>Clique aqui</TextButton>
         <FlatList
           data={this.state.feed}
           keyExtractor={(item) => item.id}
           renderItem={({item}) => <Pessoa data={item} />} 
-        />
+          />
       </View>
     );
   }
@@ -52,6 +55,17 @@ const styles = StyleSheet.create({
 
 export default App;
 
+class Img extends Component{
+  render(){
+    let img = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1VILjHSS3otgGvNMpCqrNMzMeZ4Sb1et2nQ&s'
+    
+    return(
+      <View style={styles.container}>
+        <Image source={{uri: img}}/>
+      </View>
+    )
+  }
+}
 
 
 //FlatList = feed(barra de rolagem)
