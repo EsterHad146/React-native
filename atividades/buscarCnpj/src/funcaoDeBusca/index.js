ded489d0439fb96553151329a5a051823b1ea364
EsterHad146 <hadassaester146@gmail.com>
2025-02-12 16:56:57 -0300

    Update index.js

--- atividades/buscarCnpj/src/funcaoDeBusca/index.js
+++ atividades/buscarCnpj/src/funcaoDeBusca/index.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export default async function buscarDadosPorCnpj(cnpj){
     try {
-        const response = await axios.get(`https://api.invertexto.com/v1/cnpj/${cnpj}:cnpj`);
+        const response = await axios.get(`https://api.invertexto.com/v1/cnpj/${cnpj}?token=6096|NLkZ6lrLAPtzd3xXmxZ6VyLqvZalZz02`);
         return response.data;
     } catch (error) {
         console.error('Erro ao buscar os dados cadastrados: ', error);
