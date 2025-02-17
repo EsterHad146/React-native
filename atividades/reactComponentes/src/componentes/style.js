8f7c13a4f287faf8ca8f5977e394f3c90e05d026
EsterHad146 <hadassaester146@gmail.com>
2025-02-17 16:41:28 -0300

    Update style.js

--- atividades/reactComponentes/src/componentes/style.js
+++ atividades/reactComponentes/src/componentes/style.js
@@ -8,7 +8,22 @@ const styles = StyleSheet.create({
         justifyContent:'center',
         marginVertical: 40,
         color:'#222'
+    },
+    button:{
+        backgroundColor:'#4A90E2',
+        paddingVertical:12,
+        paddingHorizontal:20,
+        borderRadius:8,
+        alignItems:'center',
+        justifyContent:'center',
+        shadowColor:'#222',
+        shadowOffset:{width:0,height:2},
+        shadowOpacity: 0.2,
+        shadowRadius:4,
+        elevation:5,
+        margin:20
     }
+
 })
 
 export default styles
